1cc6c0a8d46a3cf97ab04c6631b3308c52740365
Harsh4r0ra <harsh.arora0605+git@gmail.com>
2025-02-02 09:40:04 +0530

    base test cases created

--- crm_test/tests/Unit/Models/CompanyTest.php
+++ crm_test/tests/Unit/Models/CompanyTest.php
@@ -1,7 +1,13 @@
 <?php
-
 // tests/Unit/Models/CompanyTest.php
+
 namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+use App\Models\Company;
+use App\Models\User;
+use App\Models\Contact;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CompanyTest extends TestCase
 {
@@ -9,18 +15,16 @@ class CompanyTest extends TestCase
 
     public function test_company_has_users()
     {
-        $company = Company::factory()
-            ->hasUsers(3)
-            ->create();
+        $company = Company::factory()->create();
+        User::factory()->count(3)->create(['company_id' => $company->company_id]);
 
         $this->assertEquals(3, $company->users->count());
     }
 
     public function test_company_has_contacts()
     {
-        $company = Company::factory()
-            ->hasContacts(3)
-            ->create();
+        $company = Company::factory()->create();
+        Contact::factory()->count(3)->create(['company_id' => $company->company_id]);
 
         $this->assertEquals(3, $company->contacts->count());
     }
